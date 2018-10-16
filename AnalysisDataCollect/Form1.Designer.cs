e22a1c145bca1a1a8b3b05defbb805b908f38e91
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-10-16 16:31:38 +0900

    ベースコード

---
+++ AnalysisDataCollect/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AnalysisDataCollect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.path_textbox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(244, 84);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(118, 41);
+            this.start.TabIndex = 0;
+            this.start.Text = "データまとめ開始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.MouseClick += new System.Windows.Forms.MouseEventHandler(this.start_MouseClick);
+            // 
+            // path_textbox
+            // 
+            this.path_textbox.AllowDrop = true;
+            this.path_textbox.Location = new System.Drawing.Point(43, 51);
+            this.path_textbox.Name = "path_textbox";
+            this.path_textbox.Size = new System.Drawing.Size(527, 19);
+            this.path_textbox.TabIndex = 1;
+            this.path_textbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.path_textbox_MouseClick);
+            this.path_textbox.DragDrop += new System.Windows.Forms.DragEventHandler(this.path_textbox_DragDrop);
+            this.path_textbox.DragEnter += new System.Windows.Forms.DragEventHandler(this.path_textbox_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ax\\pulse、もしくはax\\apnea配下の取得データフォルダを指定";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(408, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "取得データ内で1テキスト1データとなっている演算結果を１つのテキストファイルにまとめる";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 148);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.path_textbox);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "データまとめツール";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox path_textbox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AnalysisDataCollect/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalysisDataCollect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void path_textbox_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                path_textbox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void start_MouseClick(object sender, MouseEventArgs e)
+        {
+            int fileCount;
+            string path;
+            string output_path = path_textbox.Text + "/sum";
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");   // 文字コードを指定
+
+            //まとめデータ出力用のsumフォルダが既にある場合は一旦中身のファイルごと削除)
+            if (System.IO.Directory.Exists(output_path))
+            {
+                Directory.Delete(output_path, true);
+            }
+
+            //sumフォルダがない状態でサブフォルダの数を取得
+            fileCount = Directory.GetDirectories(path_textbox.Text, "*", SearchOption.TopDirectoryOnly).Length;
+
+            Directory.CreateDirectory(output_path);     //まとめデータ出力用のsumフォルダ作成
+
+            for (int i = 0; i <= fileCount-1; i++)
+            {
+                path = path_textbox.Text + "/" + i;
+                if (System.IO.Directory.Exists(path))
+                {
+                    string line = "";
+                    string fileName = "";
+
+                    /* 赤色 */
+                    // 脈拍
+                    fileName = path + "/snpk(1).txt"; 
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/snpk(1).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/snpk(1)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+                    // AC平均値
+                    fileName = path + "/ac_avg(1).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/ac_avg(1).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/ac_avg(1)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    // DC平均値
+                    fileName = path + "/dc_avg(1).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/dc_avg(1).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/dc_avg(1)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    /* 赤外 */
+                    // 脈拍
+                    fileName = path + "/snpk(2).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/snpk(2).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/snpk(2)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    // AC平均値
+                    fileName = path + "/ac_avg(2).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/ac_avg(2).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/ac_avg(2)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    // DC平均値
+                    fileName = path + "/dc_avg(2).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/dc_avg(2).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/dc_avg(2)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    // SpO2
+                    fileName = path + "/spo2(0).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/spo2(0).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/spo2(0)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+
+                    // AC平均値の比（赤色/赤外）
+                    fileName = path + "/ac_avg_ratio(0).txt";
+                    if (System.IO.File.Exists(fileName))
+                    {
+                        using (System.IO.StreamReader file = new System.IO.StreamReader(path + "/ac_avg_ratio(0).txt", System.Text.Encoding.ASCII))
+                        {
+                            line = file.ReadLine();
+
+                            // ファイルを開く
+                            StreamWriter writer = new StreamWriter(output_path + "/ac_avg_ratio(0)_sum.txt", true, enc);
+
+                            writer.Write(line + "\n");
+
+                            writer.Close();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(path + "は存在しません");
+                    return;
+                }
+            }
+            MessageBox.Show("完了しました。\n" + output_path + "に結果を出力しました。");
+        }
+
+        private void path_textbox_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void path_textbox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+
+            string[] dragFilePathArr = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            path_textbox.Text = dragFilePathArr[0];
+        }
+    }
+}
