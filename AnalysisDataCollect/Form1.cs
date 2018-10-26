f8416966805c61a086e8028be00aaed54c5e576f
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-10-26 11:56:42 +0900

    関数ヘッダ追加

--- AnalysisDataCollect/Form1.cs
+++ AnalysisDataCollect/Form1.cs
@@ -21,7 +21,13 @@ namespace AnalysisDataCollect
             InitializeComponent();
         }
 
-        /* テキストボックスクリックでフォルダ選択ダイアログ表示 */
+        /************************************************************************/
+        /* 関数名   : path_textbox_MouseClick									*/
+        /* 機能     : テキストボックスクリックでフォルダ選択ダイアログ表示		*/
+        /* 引数     : object        : sender                                    */
+        /*          : MouseEventArgs: e                                         */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void path_textbox_MouseClick(object sender, MouseEventArgs e)
         {
             DialogResult dr = folderBrowserDialog1.ShowDialog();
@@ -31,7 +37,13 @@ namespace AnalysisDataCollect
             }
         }
 
-        /* テキストボックスにドラッグ&ドロップでファイルパスを選択 */
+        /************************************************************************/
+        /* 関数名   : path_textbox_DragDrop									    */
+        /* 機能     : テキストボックスにドラッグ&ドロップでファイルパスを選択	*/
+        /* 引数     : object       : sender                                     */
+        /*          : DragEventArgs: e                                          */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void path_textbox_DragDrop(object sender, DragEventArgs e)
         {
             if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
@@ -40,12 +52,25 @@ namespace AnalysisDataCollect
             path_textbox.Text = dragFilePathArr[0];
         }
 
+        /************************************************************************/
+        /* 関数名   : path_textbox_DragEnter									*/
+        /* 機能     : テキストボックスにドラッグ&ドロップでファイルパスを選択	*/
+        /* 引数     : object       : sender                                     */
+        /*          : DragEventArgs: e                                          */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void path_textbox_DragEnter(object sender, DragEventArgs e)
         {
             e.Effect = DragDropEffects.All;
         }
 
-        /* pluseデータまとめ開始ボタン押下 */
+        /************************************************************************/
+        /* 関数名   : pluseStartButton_MouseClickAsync							*/
+        /* 機能     : pluseデータまとめ開始ボタン押下                      		*/
+        /* 引数     : object        : sender                                    */
+        /*          : MouseEventArgs: e                                         */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private async void pluseStartButton_MouseClickAsync(object sender, MouseEventArgs e)
         {
             int fileCount;
@@ -62,7 +87,13 @@ namespace AnalysisDataCollect
             msgShow(result);
         }
 
-        /* apneaデータまとめ開始ボタン押下 */
+        /************************************************************************/
+        /* 関数名   : apneaStartButton_MouseClickAsync							*/
+        /* 機能     : apneaデータまとめ開始ボタン押下		                    */
+        /* 引数     : object        : sender                                    */
+        /*          : MouseEventArgs: e                                         */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private async void apneaStartButton_MouseClickAsync(object sender, MouseEventArgs e)
         {
             int fileCount;
@@ -79,7 +110,13 @@ namespace AnalysisDataCollect
             msgShow(result);
         }
 
-        /* acceデータまとめ開始ボタン押下 */
+        /************************************************************************/
+        /* 関数名   : acceStartButton_MouseClickAsync							*/
+        /* 機能     : acceデータまとめ開始ボタン押下		                    */
+        /* 引数     : object        : sender                                    */
+        /*          : MouseEventArgs: e                                         */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private async void acceStartButton_MouseClickAsync(object sender, MouseEventArgs e)
         {
             int fileCount;
@@ -96,7 +133,12 @@ namespace AnalysisDataCollect
             msgShow(result);
         }
 
-        /* データ出力用フォルダ作成 */
+        /************************************************************************/
+        /* 関数名   : createOutputFolder						    			*/
+        /* 機能     : データ出力用フォルダ作成		                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void createOutputFolder()
         {
             output_path = path_textbox.Text + "/_sum";
@@ -109,7 +151,14 @@ namespace AnalysisDataCollect
             Directory.CreateDirectory(output_path);     //まとめデータ出力用のsumフォルダ作成
         }
 
-        /* pluseデータまとめ処理 */
+        /************************************************************************/
+        /* 関数名   : pluseCollect									            */
+        /* 機能     : pluseデータまとめ処理		                                */
+        /* 引数     : string: output_path                                       */
+        /*          : int   : fileCount                                         */
+        /* 戻り値   : true : 成功                                               */
+        /*          : false: 失敗                                               */
+        /************************************************************************/
         private bool pluseCollect(string output_path, int fileCount)
         {
             string fileName = "";
@@ -174,7 +223,14 @@ namespace AnalysisDataCollect
             return true;
         }
 
-        /* apneaデータまとめ処理 */
+        /************************************************************************/
+        /* 関数名   : apneaCollect									            */
+        /* 機能     : apneaデータまとめ処理		                                */
+        /* 引数     : string: output_path                                       */
+        /*          : int   : fileCount                                         */
+        /* 戻り値   : true : 成功                                               */
+        /*          : false: 失敗                                               */
+        /************************************************************************/
         private bool apneaCollect(string output_path, int fileCount)
         {
             string fileName = "";
@@ -222,7 +278,14 @@ namespace AnalysisDataCollect
             return true;
         }
 
-        /* acceデータまとめ処理 */
+        /************************************************************************/
+        /* 関数名   : acceCollect									            */
+        /* 機能     : acceデータまとめ処理		                                */
+        /* 引数     : string: output_path                                       */
+        /*          : int   : fileCount                                         */
+        /* 戻り値   : true : 成功                                               */
+        /*          : false: 失敗                                               */
+        /************************************************************************/
         private bool acceCollect(string output_path, int fileCount)
         {
             string fileName = "";
@@ -260,7 +323,13 @@ namespace AnalysisDataCollect
             return true;
         }
 
-        /* データまとめ処理 */
+        /************************************************************************/
+        /* 関数名   : dataCollect					            				*/
+        /* 機能     : データまとめ処理		                                    */
+        /* 引数     : string: fileName                                          */
+        /*          : string: outputFilePath                                    */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void dataCollect(string fileName, string outputFilePath)
         {
             string line = "";
@@ -280,14 +349,25 @@ namespace AnalysisDataCollect
             }
         }
 
-        /* ラベル更新 */
+        /************************************************************************/
+        /* 関数名   : labelCntUp					            				*/
+        /* 機能     : ラベル更新   		                                        */
+        /* 引数     : int: cnt                                                  */
+        /*          : int: fileCount                                            */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void labelCntUp(int cnt, int fileCount)
         {
             Counter_label.Text = (cnt + 1) + " / " + fileCount;
             Update();
         }
 
-        /* メッセージ表示 */
+        /************************************************************************/
+        /* 関数名   : msgShow                  									*/
+        /* 機能     : メッセージ表示		                                    */
+        /* 引数     : bool: result                                              */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
         private void msgShow(bool result)
         {
             if (result)
